8fb70ed3164efd2d6f3a50738fe0e6ab5d9644f3
Greg Albrecht <gba@onbeep.com>
2015-11-02 21:57:03 -0800

    rewrite

--- ss.py
+++ ss.py
@@ -3,29 +3,29 @@
 """
 Generates sunset and time-lapses.
 
-:author: Greg Albrecht <gba@onbeep.com>
-:copyright: Copyright 2014 OnBeep, Inc.
+:author: Greg Albrecht <gba@gregalbrecht.com>
+:copyright: Copyright 2015 Greg Albrecht
 :license: Apache License, Version 2.0
 :source: <https://github.com/ampledata/sunsetsunset>
 """
 
-__author__ = 'Greg Albrecht <gba@onbeep.com>'
-__license__ = 'Apache License, Version 2.0'
-__copyright__ = 'Copyright 2014 OnBeep, Inc.'
 
-
-import datetime
-import json
 import os
 import sys
 import time
 
 import astral
-import dropcam
-#import forecastio
+import requests
 import twitter
 
 
+__author__ = 'Greg Albrecht <gba@gregalbrecht.com>'
+__license__ = 'Apache License, Version 2.0'
+__copyright__ = 'Copyright 2015 Greg Albrecht'
+
+
+CAMERA = 'a46f756f5a1247469466ec8975707fbe'
+GET_IMAGE_URL = 'https://nexusapi.dropcam.com/get_image?'
 OUTPUT_DIR = 'sunsets'
 
 
@@ -44,7 +44,7 @@ def get_sunset(location=None):
 
 def capture_image(camera, seconds=None, prefix=None):
     """
-    :param camera: Dropcam Camera Object Instance.
+    :param camera: Camera UUID.
     :param seconds: EPOCH time to capture image.
     """
     seconds = seconds or str(time.time())
@@ -62,36 +62,28 @@ def capture_image(camera, seconds=None, prefix=None):
 
     image_path = os.path.join(image_dir, image_name)
 
-    camera.set_property('watermark.enabled', 0)
-
     if os.path.exists(image_path):
         image_stat = os.stat(image_path)
     else:
         image_stat = None
 
+    img_params = {
+        'uuid': camera,
+        'width': width,
+        'time': seconds
+    }
+
     if not image_stat or not image_stat.st_size:
         try:
-            camera.save_image(image_path, width=width, seconds=seconds)
-        except dropcam.ConnectionError as ex:
-            print "%s@%s (%s) Error: %s" % (camera.name, seconds, prefix, ex)
+            req = requests.get(GET_IMAGE_URL, params=img_params)
+        except Exception as ex:
+            print "%s@%s (%s) Error: %s" % (camera, seconds, prefix, ex)
             return
 
-        return image_path
-
-
-
-def get_cameras(username=None, password=None):
-    """
-    Returns dropcam cameras.
-
-    >>> get_cameras()
-    """
-    username = username or os.environ['DROPCAM_USERNAME']
-    password = password or os.environ['DROPCAM_PASSWORD']
-
-    dco = dropcam.Dropcam(username, password)
-
-    return dco.cameras()
+        if req.ok:
+            return image_path
+        else:
+            return
 
 
 def twitter_post(media, message=None):
@@ -118,8 +110,6 @@ def get_timelapse_stills(camera, ts):
     tl_start = ts - time_lapse_span
     tl_end = ts + time_lapse_span
 
-    camera.set_property('watermark.enabled', 0)
-
     while tl_start < ts:
         image_name = '.'.join([str(tl_start), 'jpg'])
         image_path = os.path.join(image_dir, image_name)
@@ -129,12 +119,18 @@ def get_timelapse_stills(camera, ts):
         else:
             image_stat = None
 
+        img_params = {
+            'uuid': camera,
+            'width': width,
+            'time': tl_start
+        }
+
         if not image_stat or not image_stat.st_size:
             try:
-                camera.save_image(image_path, width=width, seconds=tl_start)
-            except dropcam.ConnectionError as ex:
-                print "%s@%s Error: %s" % (camera.name, tl_start, ex)
-        tl_start =  tl_start + 30
+                requests.get(GET_IMAGE_URL, params=img_params)
+            except Exception as ex:
+                print "%s@%s Error: %s" % (camera, tl_start, ex)
+        tl_start = tl_start + 30
 
     while tl_end > ts:
         image_name = '.'.join([str(tl_end), 'jpg'])
@@ -145,27 +141,19 @@ def get_timelapse_stills(camera, ts):
         else:
             image_stat = None
 
+        img_params = {
+            'uuid': camera,
+            'width': width,
+            'time': tl_end
+        }
+
         if not image_stat or not image_stat.st_size:
             try:
-                camera.save_image(image_path, width=width, seconds=tl_end)
-            except dropcam.ConnectionError as ex:
-                print "%s@%s Error: %s" % (camera.name, tl_end, ex)
+                requests.get(GET_IMAGE_URL, params=img_params)
+            except Exception as ex:
+                print "%s@%s Error: %s" % (camera, tl_end, ex)
         tl_end = tl_end - 30
 
-
-def build_msg(sunset_time):
-    forecastio_api_key = os.environ['FORECASTIO_API_KEY']
-    forecast = forecastio.load_forecast(
-        forecastio_api_key, 37.8267, -122.423, sunset_time)
-    message = "Today's Sunset"
-    message_meta = "in San Francisco, CA  USA: %s Sunrise: %s" % (
-        today_sun['sunset'].strftime('%X'), today_sun['sunrise'].strftime('%X'))
-    message_forecast = "High: %s Low: %s Currently: %s" % (
-        forecast['daily']['temperatureMax'],
-        forecast['daily']['temperatureMin'],
-        forecast['currently']['temperature']
-    )
-    return ' '.join([message, message_meta, message_forecast])
 
 def main():
     message = "Today's Sunset"
@@ -179,21 +167,13 @@ def main():
     if int(sunset_time) > int(time.time()) or os.path.exists(touch_path):
         sys.exit(0)
 
-    #message = build_msg(sunset_time)
-
-    cameras = get_cameras()
-
-    for camera in cameras:
-        if 'Roof' in camera.name:
-            image = capture_image(camera=camera, seconds=sunset_time)
-            if image:
-                    tr = twitter_post(image, message)
-                    if tr:
-                        with open(touch_path, 'w') as twouch:
-                            twouch.write(str(tr))
+    image = capture_image(camera=CAMERA, seconds=sunset_time)
+    if image:
+        tr = twitter_post(image, message)
+        if tr:
+            with open(touch_path, 'w') as twouch:
+                twouch.write(str(tr))
 
 
 if __name__ == '__main__':
-    #import doctest
-    #doctest.testmod()
     sys.exit(main())
